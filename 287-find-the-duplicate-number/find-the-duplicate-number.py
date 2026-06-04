22d3dbf6570f41575b72bfdf27b494ed5dd455c6
SahajJain777 <162885544+SahajJain777@users.noreply.github.com>
2026-06-04 14:47:10 +0530

    Time: 24 ms (93.15%) | Memory: 27.2 MB (13.20%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        s = set()
+
+        for num in nums:
+            if (num in s):
+                return num
+            
+            else:
+                s.add(num)
+            
+        
