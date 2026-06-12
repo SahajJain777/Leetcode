333714bad1cf318a6085ca300c7d231dd2a2d04e
SahajJain777 <162885544+SahajJain777@users.noreply.github.com>
2026-06-12 23:39:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (55.16%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        left = 0
+        right = len(nums)- 1
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+
+                if (nums[i]==nums[j]):
+                    count += 1
+        
+
+        return count
+
+    
+
+
+
+
+
