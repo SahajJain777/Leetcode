97eb162459b9839deb05de2b6f8dba62152c6f73
SahajJain777 <162885544+SahajJain777@users.noreply.github.com>
2026-06-17 10:56:18 +0530

    Time: 39 ms (10.96%) | Memory: 18.4 MB (5.95%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,32 @@
+# class Solution:
+#     def groupAnagrams(self, strs):
+#         groups = {}
+
+#         for word in strs:
+#             key = "".join(sorted(word))
+
+#             if key not in groups:
+#                 groups[key] = []
+
+#             groups[key].append(word)
+
+#         return list(groups.values())
+
+
 class Solution:
     def groupAnagrams(self, strs):
         groups = {}
 
         for word in strs:
-            key = "".join(sorted(word))
+            # Count frequencies of 'a' to 'z'
+            count = [0] * 26
+
+            for char in word:
+                index = ord(char) - ord('a')
+                count[index] += 1
+
+            # Lists can't be dictionary keys, so convert to tuple
+            key = tuple(count)
 
             if key not in groups:
                 groups[key] = []
