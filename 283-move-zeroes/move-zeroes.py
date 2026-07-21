f6f56dfa2f913719bbafd479c0bbd69671176262
SahajJain777 <162885544+SahajJain777@users.noreply.github.com>
2026-07-21 10:13:10 +0530

    Time: 3 ms (81.96%) | Memory: 20.6 MB (23.84%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,12 +1,10 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
         left = 0
 
         for i in range(len(nums)):
-            if (nums[i]==0):
-                continue
-            else:
-                nums[left],nums[i] = nums[i],nums[left]
-                left +=1
-            
+            if nums[i] != 0:
+                nums[i],nums[left] = nums[left],nums[i]
+                left += 1
+        
+        return nums
