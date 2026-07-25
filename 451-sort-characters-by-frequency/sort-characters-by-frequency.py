0b2b57c3d6d0051950d78cc8a89884a2b6bec807
SahajJain777 <162885544+SahajJain777@users.noreply.github.com>
2026-07-25 17:43:11 +0530

    Time: 3 ms (97.73%) | Memory: 20.5 MB (23.87%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -0,0 +1,19 @@
+from collections import Counter
+import heapq
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        freq = Counter(s)
+
+        heap = []
+
+        for ch, count in freq.items():
+            heapq.heappush(heap, (-count, ch))
+
+        result = ""
+
+        while heap:
+            count, ch = heapq.heappop(heap)
+            result += ch * (-count)
+
+        return result
